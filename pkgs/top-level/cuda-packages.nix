e75b6e18c6c63ed685576521a3a8542d97db69d0
Connor Baker <connor.baker@tweag.io>
2023-09-22 04:49:17 +0000

    cudaPackages: wip refactor

--- pkgs/top-level/cuda-packages.nix
+++ pkgs/top-level/cuda-packages.nix
@@ -1,75 +1,93 @@
-{ lib
+{ cudaVersion
+, lib
 , pkgs
-, cudaVersion
 }:
 
-with lib;
-
 let
+  inherit (lib) customisation fixedPoints versions;
 
-  scope = makeScope pkgs.newScope (final: {
+  scope = customisation.makeScope pkgs.newScope (final: {
+    # Entries necessary to build the finalized cudaPackages package set.
+    inherit cudaVersion lib pkgs;
+    gpus = builtins.import ../development/cuda-modules/gpus.nix;
+    nvccCompatibilities = builtins.import ../development/cuda-modules/nvccCompatibilities.nix;
+    flags = final.callPackage ../development/cuda-modules/flags.nix { };
+    # Exposed as cudaPackages.backendStdenv.
+    # This is what nvcc uses as a backend,
+    # and it has to be an officially supported one (e.g. gcc11 for cuda11).
+    #
+    # It, however, propagates current stdenv's libstdc++ to avoid "GLIBCXX_* not found errors"
+    # when linked with other C++ libraries.
+    # E.g. for cudaPackages_11_8 we use gcc11 with gcc12's libstdc++
+    # Cf. https://github.com/NixOS/nixpkgs/pull/218265 for context
+    backendStdenv =
+      let
+        gccMajorVersion = final.nvccCompatibilities.${cudaVersion}.gccMaxMajorVersion;
+        # We use buildPackages (= pkgsBuildHost) because we look for a gcc that
+        # runs on our build platform, and that produces executables for the host
+        # platform (= platform on which we deploy and run the downstream packages).
+        # The target platform of buildPackages.gcc is our host platform, so its
+        # .lib output should be the libstdc++ we want to be writing in the runpaths
+        # Cf. https://github.com/NixOS/nixpkgs/pull/225661#discussion_r1164564576
+        nixpkgsCompatibleLibstdcxx = final.pkgs.buildPackages.gcc.cc.lib;
+        nvccCompatibleCC = final.pkgs.buildPackages."gcc${gccMajorVersion}".cc;
+      in
+      final.callPackage ../development/cuda-modules/backendStdenv.nix {
+        inherit nixpkgsCompatibleLibstdcxx nvccCompatibleCC;
+      };
+
     # Here we put package set configuration and utility functions.
-    inherit cudaVersion;
     cudaMajorVersion = versions.major final.cudaVersion;
     cudaMajorMinorVersion = lib.versions.majorMinor final.cudaVersion;
-    inherit lib pkgs;
-
     addBuildInputs = drv: buildInputs: drv.overrideAttrs (oldAttrs: {
-      buildInputs = (oldAttrs.buildInputs or []) ++ buildInputs;
+      buildInputs = (oldAttrs.buildInputs or [ ]) ++ buildInputs;
     });
   });
 
-  cutensorExtension = final: prev: let
-    ### CuTensor
+  cutensorExtension = final: prev:
+    let
+      ### CuTensor
 
-    buildCuTensorPackage = final.callPackage ../development/libraries/science/math/cutensor/generic.nix;
+      buildCuTensorPackage = final.callPackage ../development/cuda-modules/cutensor/generic.nix;
 
-    cuTensorVersions = {
-      "1.2.2.5" = {
-        hash = "sha256-lU7iK4DWuC/U3s1Ct/rq2Gr3w4F2U7RYYgpmF05bibY=";
+      cuTensorVersions = {
+        "1.2.2.5" = {
+          hash = "sha256-lU7iK4DWuC/U3s1Ct/rq2Gr3w4F2U7RYYgpmF05bibY=";
+        };
+        "1.5.0.3" = {
+          hash = "sha256-T96+lPC6OTOkIs/z3QWg73oYVSyidN0SVkBWmT9VRx0=";
+        };
       };
-      "1.5.0.3" = {
-        hash = "sha256-T96+lPC6OTOkIs/z3QWg73oYVSyidN0SVkBWmT9VRx0=";
+
+      inherit (final) cudaMajorMinorVersion cudaMajorVersion;
+
+      cutensor = buildCuTensorPackage rec {
+        version = if cudaMajorMinorVersion == "10.1" then "1.2.2.5" else "1.5.0.3";
+        inherit (cuTensorVersions.${version}) hash;
+        # This can go into generic.nix
+        libPath = "lib/${if cudaMajorVersion == "10" then cudaMajorMinorVersion else cudaMajorVersion}";
       };
-    };
+    in
+    { inherit cutensor; };
 
-    inherit (final) cudaMajorMinorVersion cudaMajorVersion;
-
-    cutensor = buildCuTensorPackage rec {
-      version = if cudaMajorMinorVersion == "10.1" then "1.2.2.5" else "1.5.0.3";
-      inherit (cuTensorVersions.${version}) hash;
-      # This can go into generic.nix
-      libPath = "lib/${if cudaMajorVersion == "10" then cudaMajorMinorVersion else cudaMajorVersion}";
-    };
-  in { inherit cutensor; };
-
-  extraPackagesExtension = final: prev: {
-
-    nccl = final.callPackage ../development/libraries/science/math/nccl { };
-
-    nccl-tests = final.callPackage ../development/libraries/science/math/nccl/tests.nix { };
-
-    autoAddOpenGLRunpathHook = final.callPackage ( { makeSetupHook, addOpenGLRunpath }:
-      makeSetupHook {
-        name = "auto-add-opengl-runpath-hook";
-        propagatedBuildInputs = [
-          addOpenGLRunpath
-        ];
-      } ../development/compilers/cudatoolkit/auto-add-opengl-runpath-hook.sh
-    ) {};
-
+  extraPackagesExtension = final: _: {
+    cudatoolkit = final.callPackage ../development/cuda-modules/cudatoolkit { };
+    nccl = final.callPackage ../development/cuda-modules/nccl { };
+    nccl-tests = final.callPackage ../development/cuda-modules/nccl-tests { };
+    saxpy = final.callPackage ../development/cuda-modules/saxpy { };
   };
 
-  composedExtension = composeManyExtensions ([
+  composedExtension = fixedPoints.composeManyExtensions ([
+    (import ../development/cuda-modules/hooks/extension.nix)
     extraPackagesExtension
-    (import ../development/compilers/cudatoolkit/extension.nix)
-    (import ../development/compilers/cudatoolkit/redist/extension.nix)
-    (import ../development/compilers/cudatoolkit/redist/overrides.nix)
-    (import ../development/libraries/science/math/cudnn/extension.nix)
-    (import ../development/libraries/science/math/tensorrt/extension.nix)
-    (import ../test/cuda/cuda-samples/extension.nix)
-    (import ../test/cuda/cuda-library-samples/extension.nix)
+    (import ../development/cuda-modules/cuda/extension.nix)
+    (import ../development/cuda-modules/cuda/overrides.nix)
+    # (import ../development/cuda-modules/cudnn/extension.nix)
+    # (import ../development/cuda-modules/tensorrt/extension.nix)
+    # (import ../test/cuda/cuda-samples/extension.nix)
+    # (import ../test/cuda/cuda-library-samples/extension.nix)
     cutensorExtension
   ]);
 
-in (scope.overrideScope composedExtension)
+in
+(scope.overrideScope composedExtension)
