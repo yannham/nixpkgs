98f1bb8ebaa053ac549830dc15ef502581660834
Connor Baker <connor.baker@tweag.io>
2023-10-17 13:20:19 +0000

    cudaPackages.nccl-tests: enable builds on platforms.linux

--- pkgs/development/cuda-modules/nccl-tests/default.nix
+++ pkgs/development/cuda-modules/nccl-tests/default.nix
@@ -51,7 +51,7 @@ backendStdenv.mkDerivation (finalAttrs: {
   meta = with lib; {
     description = "Tests to check both the performance and the correctness of NVIDIA NCCL operations";
     homepage = "https://github.com/NVIDIA/nccl-tests";
-    platforms = [ "x86_64-linux" ];
+    platforms = platforms.linux;
     license = licenses.bsd3;
     broken = !config.cudaSupport || (mpiSupport && mpi == null);
     maintainers = with maintainers; [ jmillerpdt ];
