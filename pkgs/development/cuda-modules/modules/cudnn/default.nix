10857d528f7a6f5271e768be41612dba7c188b3c
Yann Hamdaoui <yann.hamdaoui@tweag.io>
2023-10-19 15:53:22 +0200

    Split release modules defs
    The releases option set by various CUDA libraries (cudnn, tensorrt,
    etc.) share a common base but each adds a few different options. This
    commit factors the commonalities in a separate submodule, that is then
    used (in conjunction with `submoduleWith`) to combine an extension and
    generate the corresponding config options without code duplication.

--- pkgs/development/cuda-modules/modules/cudnn/default.nix
+++ pkgs/development/cuda-modules/modules/cudnn/default.nix
@@ -1,11 +1,26 @@
-{options, ...}: {
-  options.cudnn.releases = options.generic.releases;
-  # TODO(@connorbaker): Figure out how to add additional options to the
-  # to the generic release.
-  # {
-  #   url = options.mkOption {
-  #     description = "The URL to download the tarball from";
-  #     type = types.str;
-  #   };
-  # }
+{ options, types, ... }:
+{
+  options.cudnn.releases = options.mkOption
+    {
+      description = "A collection of cudnn packages targeting different platforms";
+      type =
+        # Additional options to the base package type that are specific to
+        # cudnn.
+        let cudnnPackageExtension = {
+          options.url = options.mkOption {
+            description = "The URL to download the tarball from";
+            type = types.str;
+          };
+        };
+        in
+        let extendedPackageType = types.submoduleWith {
+          modules = [
+            config.generic.releases
+            cudnnPackageExtension
+          ];
+          shorthandOnlyDefinesConfig = true;
+        };
+        in
+        types.attrsOf (types.listOf extendedPackageType);
+    };
 }
--- pkgs/development/cuda-modules/modules/tensorrt/default.nix
+++ pkgs/development/cuda-modules/modules/tensorrt/default.nix
@@ -1,15 +1,32 @@
-{options, ...}: {
-  options.tensorrt.releases = options.generic.releases;
-  # TODO(@connorbaker): Figure out how to add additional options to the
-  # to the generic release.
-  # {
-  #   cudnnVersion = lib.options.mkOption {
-  #     description = "The CUDNN version supported";
-  #     type = types.nullOr majorMinorVersion;
-  #   };
-  #   filename = lib.options.mkOption {
-  #     description = "The tarball name";
-  #     type = types.str;
-  #   };
-  # }
+{ options, types, ... }:
+{
+  options.tensorrt.releases = options.mkOption
+    {
+      description = "A collection of tensorrt packages targeting different platforms";
+      type =
+        # Additional options to the base package type that are specific to
+        # cudnn.
+        let tensorrtPackageExtension = {
+          options = {
+            cudnnVersion = lib.options.mkOption {
+              description = "The CUDNN version supported";
+              type = types.nullOr majorMinorVersion;
+            };
+            filename = lib.options.mkOption {
+              description = "The tarball name";
+              type = types.str;
+            };
+          };
+        };
+        in
+        let extendedPackageType = types.submoduleWith {
+          modules = [
+            config.generic.releases
+            tensorrtPackageExtension
+          ];
+          shorthandOnlyDefinesConfig = true;
+        };
+        in
+        types.attrsOf (types.listOf extendedPackageType);
+    };
 }
